8207e22eb9ec09033df69e204a6686fa39d9dabb
Vishal-rana-dot <vishal.kumar@bellblazetech.com>
2026-01-30 09:50:28 +0530

    Add files via upload
    python-day1-task

---
+++ app.py
@@ -0,0 +1,25 @@
+nums = [7, 12, 3, 9, 20]
+
+# suppose I want to add 30 at index 1
+nums.insert(1, 30)
+print(nums)  # printing the list
+
+# now removing the first element
+nums.pop(0)
+print(nums)
+
+# adding a number at the end of the list
+nums.append(50)
+print(nums)
+
+# sorting the list
+nums.sort()
+print(nums)
+
+# removing the last element
+nums.pop()
+print(nums)
+
+# reversing the list
+nums.reverse()
+print(nums)
